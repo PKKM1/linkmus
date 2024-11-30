c378e1aec7847d60b000048091fcfac382454500
egorvostochny <hovachik337@gmail.com>
2024-12-01 02:00:43 +0300

    Delete docs directory

--- docs/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".social-links .button");
-  buttons.forEach(button => button.classList.add("show"));
-});
